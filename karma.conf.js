40aeeab7be8ca17a1329a73b13544499abfac311
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2022-06-26 22:17:13 +0300

    update angular version 11 to 14

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/eCommerceClient'),
+      dir: require('path').join(__dirname, './coverage/e-commerce-client'),
       subdir: '.',
       reporters: [
         { type: 'html' },
