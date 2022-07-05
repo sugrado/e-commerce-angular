f305d8570829a9da056993e5c4992df7e1a686a9
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2022-07-05 10:50:15 +0300

    material button and icon required modules imported

--- src/app/pages/admin/components/products/products.module.ts
+++ src/app/pages/admin/components/products/products.module.ts
@@ -2,9 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AllProductsComponent } from './all-products/all-products.component';
 import { ProductsRoutingModule } from './products.routing';
+import { NewProductComponent } from './new-product/new-product.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [AllProductsComponent],
-  imports: [CommonModule, ProductsRoutingModule],
+  declarations: [AllProductsComponent, NewProductComponent],
+  imports: [
+    CommonModule,
+    ProductsRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
 export class ProductsModule {}
