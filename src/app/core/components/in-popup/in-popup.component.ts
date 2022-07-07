30b6cb5ed3fb867de124080e5fe75ece652804e5
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2022-07-07 22:07:08 +0300

    created core module and in-popup component files

---
+++ src/app/core/components/in-popup/in-popup.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'in-popup',
+  templateUrl: './in-popup.component.html',
+  styleUrls: ['./in-popup.component.scss'],
+})
+export class InPopupComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InPopupComponent } from './components/in-popup/in-popup.component';
+
+@NgModule({
+  declarations: [InPopupComponent],
+  imports: [CommonModule],
+  exports: [InPopupComponent],
+  providers: [],
+})
+export class CoreModule {}
