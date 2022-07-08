1121ca16ddb05acd2f72efa9a06b1c1f884654ed
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2022-07-08 19:57:19 +0300

    created dynamic in-popup component

--- src/app/core/components/in-popup/in-popup.component.ts
+++ src/app/core/components/in-popup/in-popup.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 
 @Component({
   selector: 'in-popup',
@@ -6,7 +15,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./in-popup.component.scss'],
 })
 export class InPopupComponent implements OnInit {
-  constructor() {}
+  @ViewChild('popupContent', { static: false })
+  popupContent!: TemplateRef<any>;
+
+  @Input() title: string = 'Default Popup Title';
+  @Input() width: string = '500px';
+  @Input() height: string = '500px';
+  @Input() maxWidth: string = '95vw';
+  @Input() maxHeight: string = '95vh';
+  @Input() closeOnOutsideClick: boolean = false;
+  @Input() showCloseButton: boolean = true;
+  @Input() fullSecreen: boolean = false;
+
+  @Output() onClosed = new EventEmitter();
+  _visible: boolean = false;
+  @Input() set visible(val: boolean) {
+    this._visible = val;
+    val ? this.openPopup() : this.closePopup();
+  }
+
+  constructor(private dialog: MatDialog) {}
 
   ngOnInit() {}
+
+  openPopup() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = !this.closeOnOutsideClick;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = this.fullSecreen ? '95vw' : this.width;
+    dialogConfig.height = this.fullSecreen ? '95vh' : this.height;
+    dialogConfig.maxWidth = this.fullSecreen ? '95vw' : this.maxWidth;
+    dialogConfig.maxHeight = this.fullSecreen ? '95vh' : this.maxHeight;
+
+    this.dialog.open(this.popupContent, dialogConfig);
+  }
+
+  closePopup() {
+    let lastDialogId =
+      this.dialog.openDialogs[this.dialog.openDialogs.length - 1]?.id;
+    this.dialog.getDialogById(lastDialogId)?.close();
+    this.onClosed.emit();
+  }
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InPopupComponent } from './components/in-popup/in-popup.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [InPopupComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule, MatToolbarModule, MatDialogModule],
   exports: [InPopupComponent],
   providers: [],
 })
