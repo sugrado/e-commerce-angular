53e3a4c77277f234f3a4ef39ca912970d6f75ecb
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2022-07-18 21:40:50 +0300

    spinner-environment.showSpinner connection bugfix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewChecked,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
 import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewChecked {
   title = 'eCommerceClient';
   environment = environment;
+
+  constructor(private cd: ChangeDetectorRef) {}
+
+  ngAfterViewChecked() {
+    this.cd.detectChanges();
+  }
 }
